b3a7b5e39d586e978784d05fc71c41368332183a
SHANTANU PATIL <shantanupatil262002@gmail.com>
2025-05-19 02:41:58 +0800

    Positive Negative Number Check

---
+++ IFELSEIF.py
@@ -0,0 +1,9 @@
+
+#Positive Negative Number Check
+x=int(input('Please Enter a number'))
+if x>0:
+    print('is a Positive Number',x)
+elif x<0:
+    print('is a Negative Number')
+else:
+    print('Number is zero')
