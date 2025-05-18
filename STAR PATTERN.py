1bc07d005e21d34ad3e4c94a0a26cc7128f0748c
SHANTANU PATIL <shantanupatil262002@gmail.com>
2025-05-19 02:57:49 +0800

    Star pattern

---
+++ STAR PATTERN.py
@@ -0,0 +1,22 @@
+for i in range(5):
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+for i in range(5,0,-1):
+    for j in range(i,0,-1):
+        print("*", end=" ")
+    print()
+
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+
